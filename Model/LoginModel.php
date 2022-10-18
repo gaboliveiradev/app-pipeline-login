639488cb4c88fefed02cb5015285aad42300d660
gaboliveiradev <g38789138@gmail.com>
2022-10-18 20:56:36 -0300

    criando cookie caso o usuário marque a opção "lembrar de mim"

--- Model/LoginModel.php
+++ Model/LoginModel.php
@@ -7,7 +7,7 @@ use App\DAO\LoginDAO;
 class LoginModel extends Model {
 
     public $id, $nome, $email, $senha, $remember = false;
-    public $user_cookie;
+    public $admin_cookie;
 
     public function auth() {
         $dao = new LoginDAO();
@@ -15,9 +15,15 @@ class LoginModel extends Model {
 
         if($data_user) {
             $_SESSION["admin_logged"] = $data_user;
+            if($this->remember) self::remember($this->nome);
             header("Location: /dashboard");
         } else {
             header("Location: /login");
         }
+    }
+
+    private static function remember($nome) {
+        $validate = strtotime("+1 month");
+        setcookie("admin_login", $nome, $validate, "/", "", false, true);
     }
 }

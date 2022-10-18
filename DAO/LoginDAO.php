f190afe03fdd87eab18e6b3de50d447487a21f46
gaboliveiradev <g38789138@gmail.com>
2022-10-18 20:01:27 -0300

    verificando se o login é valido

--- DAO/LoginDAO.php
+++ DAO/LoginDAO.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\DAO;
+
+use App\Model\LoginModel;
 use \PDO;
 
 class LoginDAO extends DAO {
@@ -11,12 +13,12 @@ class LoginDAO extends DAO {
         parent::__construct();  
     } 
 
-    public function selectEmailAndSenhaByAdminForLogin(string $email, string $senha) {
+    public function selectEmailAndSenhaByAdminForLogin(LoginModel $model) {
         $sql = "SELECT * FROM admin WHERE email = ? AND senha = ?";
 
         $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $email);
-        $stmt->bindValue(2, $senha);
+        $stmt->bindValue(1, $model->email);
+        $stmt->bindValue(2, sha1($model->senha));
         $stmt->execute();
 
         return $stmt->fetchObject();
--- Model/LoginModel.php
+++ Model/LoginModel.php
@@ -2,12 +2,22 @@
 
 namespace App\Model;
 
+use App\DAO\LoginDAO;
+
 class LoginModel extends Model {
 
     public $id, $nome, $email, $senha, $remember = false;
     public $user_cookie;
 
     public function auth() {
+        $dao = new LoginDAO();
+        $data_user = $dao->selectEmailAndSenhaByAdminForLogin($this);
 
+        if($data_user) {
+            $_SESSION["admin_logged"] = $data_user;
+            header("Location: /dashboard");
+        } else {
+            header("Location: /login");
+        }
     }
 }

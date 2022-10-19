4ce2261a39394ff02e26dc9456874a096d0e09a4
gaboliveiradev <g38789138@gmail.com>
2022-10-19 13:52:47 -0300

    criando os arquivos onde será exibido as lista de empresas, vagas e curriculos

---
+++ App/View/Modules/Empresa/ListaEmpresas.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "./View/Includes/Bootstrap/css_config.php" ?>
+    <title>Listas de Empresas - Painel Admin</title>
+</head>
+<body>
+    <header>
+        <?php include "./View/Includes/header.php" ?>
+    </header>
+    <h1>Lista de Empresas</h1>
+
+    <?php include "./View/Includes/Bootstrap/js_config.php" ?>
+</body>
+</html>

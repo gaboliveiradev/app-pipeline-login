0e58b8ab796220f8b08257266b7bcefdd0b7e795
gaboliveiradev <g38789138@gmail.com>
2022-10-19 12:25:07 -0300

    estilizando a área protegida (dashboard)

--- App/View/Includes/header.php
+++ App/View/Includes/header.php
@@ -1,1 +1,40 @@
-//CODEFLAME
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container">
+        <button class="navbar-toggler text-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+            <h5>Jahu Banco de Empregos</h5>
+            <!--<img src="./../../View/img/logo.png" alt="mdo" width="52" height="52" class="d-inline-block align-text-top">-->
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item m-3">
+                    <a class="nav-link active text-dark" aria-current="page" href="/">Empresas</a>
+                </li>
+                <li class="nav-item m-3">
+                    <a class="nav-link text-dark" href="/">Curriculos</a>
+                </li>
+                <li class="nav-item m-3">
+                    <a class="nav-link text-dark" href="/">Vagas de Empregos</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+        <!-- Dropdown do User -->
+        <div class="navbar-brand dropdown text-end p-2 ">
+            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                <?= $_SESSION['admin_logged']->nome ?>
+            </a>
+            <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                <li><a class="dropdown-item" href="/">Minha Conta</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="/logout?exit=true">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- App/View/Modules/Dashboard/MenuInicial.php
+++ App/View/Modules/Dashboard/MenuInicial.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "./View/Includes/Bootstrap/css_config.php" ?>
     <title>Dashboard - Logado como <?= $_SESSION['admin_logged']->nome ?></title>
 </head>
 <body>
-    <h1>ÁREA PROTEGIDA</h1>
-    <a href="/logout?exit=true">Fazer Loggout</a>
+    <header>
+        <?php include "./View/Includes/header.php" ?>
+    </header>
+
+    <?php include "./View/Includes/Bootstrap/js_config.php" ?>
 </body>
 </html>

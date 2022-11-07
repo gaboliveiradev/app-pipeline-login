6e45ffc7064249c6fc50d67c74e189a21502b814
gaboliveiradev <g38789138@gmail.com>
2022-11-06 21:53:07 -0300

    finalizações

--- App/View/Modules/Funcionario/Form.php
+++ App/View/Modules/Funcionario/Form.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include "./View/Includes/Bootstrap/css_config.php" ?>
-    <title>Jahu Banco de Empregos | Painel Admin</title>
+    <title>Jahu Banco de Empregos | Cadastrar Funcionários</title>
     <style>
         main {
             width: 100vw;

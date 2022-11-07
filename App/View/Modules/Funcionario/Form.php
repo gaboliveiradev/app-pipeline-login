8d6d31e2ed90aec105de0847add53a61a1c0ac22
gaboliveiradev <g38789138@gmail.com>
2022-11-06 21:22:35 -0300

    estilizando o formulário para cadastrar funcionario

---
+++ App/View/Modules/Funcionario/Form.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "./View/Includes/Bootstrap/css_config.php" ?>
+    <title>Jahu Banco de Empregos | Painel Admin</title>
+    <style>
+        body {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: row;
+        }
+
+        .frm_login {
+            width: 60vw;
+        }
+
+        .btn {
+            width: 15vw;
+        }
+    </style>
+</head>
+<body class="text-center flex">
+    <main class="container flex frm_login">
+        <div class="form-signin flex">
+            <form action="/login/auth" method="POST">
+                <h1 class="h3 mb-3 fw-normal">Cadastrar Funcionários</h1>
+
+                <div class="form-floating" class="col-md-6 offset-md-3">
+                    <input name="nome" type="text" class="form-control" id="text" placeholder="name@example.com">
+                    <label for="floatingInput">Nome</label>
+                </div>
+                <br>
+                <div class="form-floating" class="col-md-6 offset-md-3">
+                    <input name="email" type="text" class="form-control" id="email" placeholder="name@example.com">
+                    <label for="floatingInput">Email</label>
+                </div>
+                <br>
+                <div class="form-floating">
+                    <input name="senha" type="password" class="form-control" id="Senha" placeholder="Password">
+                    <label for="floatingPassword">Senha</label>
+                </div>
+
+                <button type="submit" class="btn btn-lg btn-primary">Cadastrar</button>
+                <p class="mt-5 mb-3 text-muted">&copy; 2022–2023</p>
+            </form>
+        </div>
+    </main>
+    
+    <?php include "./View/Includes/Bootstrap/js_config.php" ?>
+</body>
+</html>
